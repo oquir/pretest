2d0c299f0f89f4e74346c817c430face168f3b33
manaranjoc <angel.naranjo@udea.edu.co>
2022-03-19 09:19:51 -0500

    Add basic structure tests

--- src/main/java/com/tdea/pretest/UserController.java
+++ src/main/java/com/tdea/pretest/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     UserService userService;
--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -1,13 +1,11 @@
 package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PretestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
